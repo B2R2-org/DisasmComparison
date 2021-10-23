7f6d3ee2047a47a3e466091138f58ce9695a9ffe
Sang Kil Cha <sangkilc@kaist.ac.kr>
2021-10-23 20:10:28 +0900

    Use nuget

--- b2r2_test/b2r2_test/Program.fs
+++ b2r2_test/b2r2_test/Program.fs
@@ -3,17 +3,17 @@ open B2R2
 open B2R2.FrontEnd.BinInterface
 open B2R2.FrontEnd.BinFile
 
-let rec disas hdl ctxt bp (addr: Addr) =
+let rec disas hdl bp (addr: Addr) =
   if BinaryPointer.IsValid bp then
-    match BinHandle.TryParseInstr (hdl, ctxt, bp=bp) with
+    match BinHandle.TryParseInstr (hdl, bp=bp) with
     | Ok (ins) ->
       let len = ins.Length
       Console.WriteLine (addr.ToString ("X8") + " " + ins.Disasm ())
       let bp' = BinaryPointer.Advance bp (int len)
-      disas hdl ctxt bp' (addr + uint64 len)
+      disas hdl bp' (addr + uint64 len)
     | Error _ ->
       let bp' = BinaryPointer.Advance bp 1
-      disas hdl ctxt bp' (addr + 1UL)
+      disas hdl bp' (addr + 1UL)
   else ()
 
 [<EntryPoint>]
@@ -30,7 +30,7 @@ let main argv =
 
   let sw = Diagnostics.Stopwatch.StartNew ()
   let bp = hdl.FileInfo.ToBinaryPointer hdl.FileInfo.BaseAddress
-  disas hdl hdl.DefaultParsingContext bp 0UL
+  disas hdl bp 0UL
   sw.Stop ()
   Console.Error.WriteLine ("Total time: {0:F} sec", sw.Elapsed.TotalSeconds)
   0
